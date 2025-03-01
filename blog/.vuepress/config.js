60271966fc5fd77e2ab1a0dd3e3227b8d824478d
Miguel Chen <vvchan@outlook.com>
2025-02-28 18:51:55 -0800

    update names

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -1,35 +1,59 @@
 module.exports = {
-  title: 'Miguel\'s Blog',
+  title: "M's Blog",
   description: 'Well, hello.',
   theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
   // theme: "blog-vuetify",
 
   markdown: {
-    extendMarkdown: md => {
-      md.use(require("markdown-it-katex"));
-    }
+    extendMarkdown: (md) => {
+      md.use(require('markdown-it-katex'));
+    },
   },
 
   head: [
     // Adsense
-    ['script', {
-      'data-ad-client': 'ca-pub-9539082383597390',
-      async: true,
-      src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'
-    }],
+    [
+      'script',
+      {
+        'data-ad-client': 'ca-pub-9539082383597390',
+        async: true,
+        src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+      },
+    ],
     // Google Analytics
-    ['script', {
-      async: true,
-      src: 'https://www.googletagmanager.com/gtag/js?id=UA-135644242-1'
-    }],
-    ['script', {}, `
+    [
+      'script',
+      {
+        async: true,
+        src: 'https://www.googletagmanager.com/gtag/js?id=UA-135644242-1',
+      },
+    ],
+    [
+      'script',
+      {},
+      `
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-135644242-1');
-    `],
-    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css' }]
+    `,
+    ],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdn.jsdelivr.net/github-markdown-css/2.2.1/github-markdown.css',
+      },
+    ],
   ],
 
   themeConfig: {
@@ -37,7 +61,7 @@ module.exports = {
      * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
      */
     modifyBlogPluginOptions(blogPluginOptions) {
-      return blogPluginOptions
+      return blogPluginOptions;
     },
     /**
      * Ref: https://vuepress-theme-blog.ulivz.com/#nav
@@ -78,4 +102,4 @@ module.exports = {
       ],
     },
   },
-}
+};
